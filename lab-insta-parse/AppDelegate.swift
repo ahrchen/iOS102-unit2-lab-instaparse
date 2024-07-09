5c631a3b9045545a6ae532b25de2f94ab9449b2d
Raymond Chen <ahrchen@gmail.com>
2024-07-08 22:10:46 -0700

    Step 3: Parse User - Log out

--- lab-insta-parse/AppDelegate.swift
+++ lab-insta-parse/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        // TODO: Pt 1 - Initialize Parse SDK
         // https://github.com/parse-community/Parse-Swift/blob/main/ParseSwift.playground/Sources/Common.swift
         ParseSwift.initialize(applicationId: "IZ8Oq5Qu8b7lmqQTfELevJBfigQaIuqku3iakxiq",
                               clientKey: "hzWUzIAPencoIJXuhSPJButdepQpubahlfa2MITq",
